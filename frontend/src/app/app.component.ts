5de4fda4824a6815125688897bfcca661e0d59ba
samirpulatov <samirpulatov42@gmail.com>
2025-05-30 11:38:27 +0200

    ^ changed database-icon

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit, OnDestroy {
         { icon: 'groups', label: 'Nutzerverwaltung', route: '/users', requiredRoles: [Role.TUTOR] },
         { icon: 'timeline', label: 'Roadmap', route: '/roadmap', requiredRoles: [Role.TUTOR] },
         {
-            icon: 'database',
+            icon: 'storage',
             label: 'Datenbanken',
             route: '/databases',
             requiredRoles: [Role.TUTOR],
